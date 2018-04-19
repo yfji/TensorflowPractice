13a82933983178519f7a2d8fc79291de5f75b833
yfji <jyf131462@163.com>
2018-04-19 18:36:38 +0800

    fix bug for mis-crop in rotate

--- data_augment.py
+++ data_augment.py
@@ -30,6 +30,7 @@ def aug_rotate(image, rot_max, keypoints):
     sin,cos=np.sin(rad),np.cos(rad)
     R=np.asarray([[cos,sin],[-sin,cos]])
     corners=np.asarray([[0,0],[w,0],[w,h],[0,h]])
+    
     t_corners=corners.dot(R.T).astype(np.int32)
     
     keypoints[:,0:2]=keypoints[:,0:2].dot(R.T)
@@ -39,29 +40,31 @@ def aug_rotate(image, rot_max, keypoints):
     center=(w*0.5, h*0.5)
     R=cv2.getRotationMatrix2D(center, angle, 1)
     R[0,2]+=nw/2-w/2
-    R[1,2]=nh/2-h/w
+    R[1,2]+=nh/2-h/2
     img_rot=cv2.warpAffine(image, R, (nw,nh), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(128,128,128))
     
     keypoints[:,0]+=R[0,2]
     keypoints[:,1]+=R[1,2]
     return img_rot
 
-def aug_crop(image, net_input_size, center_perterb_max, keypoints):
+def aug_crop(image, center, net_input_size, center_perterb_max, keypoints):
     probs=np.random.random(size=2)
-    mw,mh=int(image.shape[1]/2-0.5), int(image.shape[0]/2-0.5)
-    dx,dy=int((probs[0]-0.5)*center_perterb_max*2), int((probs[1]-0.5)*center_perterb_max*2)
+    #(mx,my): any point on image
+    mx,my=int(center[0]-0.5),int(center[1]-0.5)
+    dx,dy=int((2*probs[0]-1)*center_perterb_max), int((2*probs[1]-1)*center_perterb_max)
+    #(cx,cy): any point on canvas
     cx,cy=int(net_input_size/2),int(net_input_size/2)
     
     crop_image=128*np.ones((net_input_size,net_input_size,3),dtype=np.float32)
     
-    startx1=max(0,cx-mw+dx)
-    starty1=max(0,cy-mh+dy)
-    endx1=min(net_input_size, cx+mw+dx)
-    endy1=min(net_input_size, cy+mh+dy)
-    startx2=max(0,mw-dx-cx)
-    starty2=max(0,mh-dy-cy)
-    endx2=min(2*mw,mw-dx+cx)
-    endy2=min(2*mh,mh-dy+cy)
+    startx1=max(0,cx-mx+dx)
+    starty1=max(0,cy-my+dy)
+    endx1=min(net_input_size, cx+(image.shape[1]-mx)+dx)
+    endy1=min(net_input_size, cy+(image.shape[0]-my)+dy)
+    startx2=max(0,mx-dx-cx)
+    starty2=max(0,my-dy-cy)
+    endx2=min(image.shape[1],mx-dx+(net_input_size-cx))
+    endy2=min(image.shape[0],my-dy+(net_input_size-cy))
     
     keypoints[:,0]+=(startx1-startx2)
     keypoints[:,1]+=(starty1-starty2)
